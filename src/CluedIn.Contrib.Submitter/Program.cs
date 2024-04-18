fc293dd058ad06b13c12205f84f7c9e84848f008
romaklimenko <romaklimenko@gmail.com>
2024-04-18 13:21:22 +0200

    Authentication & Authorization

--- src/CluedIn.Contrib.Submitter/Program.cs
+++ src/CluedIn.Contrib.Submitter/Program.cs
@@ -1,11 +1,42 @@
 using CluedIn.Contrib.Submitter.WebApi;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        var authUrl = Environment.GetEnvironmentVariable("AUTH_API") ?? "http://cluedin-server:9001/";
+        options.Authority = authUrl;
+        options.Audience = "PublicApi";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = authUrl, ValidateIssuerSigningKey = true
+        };
+        options.SaveToken = true;
+    });
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("api_token", policy =>
+    {
+        policy.RequireAuthenticatedUser()
+            .RequireRole("API")
+            .RequireClaim("scope", ["PublicApi", "ServerApiForUI"]);
+    });
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/index.html", () => Results.LocalRedirect("/", true, true));
 
-app.MapPost("/data", Endpoints.SubmitData);
+app.MapPost("/data", Endpoints.SubmitData).RequireAuthorization("api_token");
+
 
 app.Run();
